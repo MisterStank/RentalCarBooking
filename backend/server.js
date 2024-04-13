feeaeed201fbebce3ec36ca23f37511d0591a9f3
MisterStank <chakkritchongkraichak@gmail.com>
2024-04-13 21:07:20 +0700

    feat(auth): :sparkles: auth

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
-// const nodemailer = require("nodemailer");
+const cars = require("./routes/cars");
+const auth = require("./routes/auth");
+const bookings = require("./routes/bookings");
+const cookieParser = require("cookie-parser");
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
+
+app.use("/api/v1/auth", auth);
+
+
 
 const PORT = process.env.PORT || 5001;
 const server = app.listen(
